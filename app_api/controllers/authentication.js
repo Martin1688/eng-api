4c27c189682c6f429ce246c985578506780c9959
martin <da5407@gmail.com>
2022-11-14 13:23:08 +0800

    debug call API failed from chrome

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -23,12 +23,12 @@ const register = (req, res) => {
         if (err) {
             res
                 .status(400)
-                .json(err);
+                .json({ "message": err,"data":"" });
         } else {
             const token = user.generateJwt();
             res
                 .status(200)
-                .json({ 'token': token, 'name': user.name, 'grade': user.grade });
+                .json({ "message": "","data": { 'token': token, 'name': user.name, 'grade': user.grade }});
         }
     })
 };
@@ -37,23 +37,23 @@ const login = (req, res) => {
     if (!req.body.email || !req.body.password) {
         return res
             .status(400)
-            .json({ "message": "email and password required" });
+            .json({ "message": "email and password required","data":"" });
     }
     passport.authenticate('local', (err, user, info) => {
         if (err) {
             return res
                 .status(404)
-                .json(err);
+                .json({ "message": err,"data":"" });
         }
         if (user) {
             const token = user.generateJwt();
             res.set({ 'content-type': 'application/json; charset=utf-8' })
                 .status(200)
-                .json({ 'token': token, 'name': user.name, 'grade': user.grade });
+                .json({ "message": "","data":{ 'token': token, 'name': user.name, 'grade': user.grade } });
         } else {
             res
                 .status(400)
-                .json(info);
+                .json({ "message": info,"data":"" });
         }
     })(req, res);
 };
